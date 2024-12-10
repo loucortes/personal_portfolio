09ce2e6c8e6486af6702e2e7f40cccbd54f74650
loucortes <lucianohcortes@gmail.com>
2024-12-10 19:12:32 -0300

    Add: navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <p>Luciano Cortés</p>
+
+      <ul className='nav-items'>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
