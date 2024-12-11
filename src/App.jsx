89341d3cf7bf83a0522c1280a013923ebc5c26ac
loucortes <lucianohcortes@gmail.com>
2024-12-10 22:43:07 -0300

    Add modifications

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Navbar from './Navbar/Navbar'
 function App() {
   return (
     <>
+      <div className='blur'>
+      </div>
+      <div className='blur2'>
+      </div>
+
       <Navbar />
     </>
   )
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
       <p>Luciano Cortés</p>
 
       <ul className='nav-items'>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li><a href="">About</a></li>
+        <li><a href="">Skills</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Contact</a></li>
       </ul>
     </nav>
   )
