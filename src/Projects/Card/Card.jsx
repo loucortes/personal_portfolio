c9b1c022ca3ba58dff32077355087a1632ef2be9
loucortes <lucianohcortes@gmail.com>
2024-12-12 13:43:24 -0300

    fix: images for vercel deployment

--- src/Projects/Card/Card.jsx
+++ src/Projects/Card/Card.jsx
@@ -3,7 +3,7 @@ function Card() {
   return (
     <div className='card'>
       <div className="card-container">
-        <img src="src\assets\MainAfter.jpg" alt="" />
+        <img src="images/MainAfter.jpg" alt="" />
 
         <div className="card-description">
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit doloribus voluptas rerum quis modi, ab laudantium mollitia dolore. Earum, quia!</p>
@@ -13,9 +13,9 @@ function Card() {
 
             <div className='card-technologies'>
               <img src="images/javascript-logo-svgrepo-com.svg" alt="" />
-              <img src="src\assets\react-svgrepo-com.svg" alt="" />
-              <img src="src\assets\node-js-svgrepo-com.svg" alt="" />
-              <img src="src\assets\postgresql-svgrepo-com.svg" alt="" />
+              <img src="images/react-svgrepo-com.svg" alt="" />
+              <img src="images/node-js-svgrepo-com.svg" alt="" />
+              <img src="images/postgresql-svgrepo-com.svg" alt="" />
             </div>
 
           </div>
